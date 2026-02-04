a8cf40cad383dea721f6b4aacc2e1fa63b7af485
VJADOUN2000 <jadounvikrant@gmail.com>
2026-02-04 23:43:25 +0530

    Understanding Module export & import in script

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 var h1 = document.createElement("h1")
 
-console.log(h1);
+h1.innerHTML = "Hello from JS!"
+
+
+document.body.appendChild(h1)
+console.log(a);
+
+// Library --> GSAPM, Lenis & REACTJS   ---> for particular feature
+// Framework ---> NEXTJS, Angular, Remix  --> 
+
+import a from './app'
