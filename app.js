b0765a85c87f0e078c3101ff45b27b5e7fdf6cdf
vikrant singh <jadounvikrant@example.com>
2026-02-05 18:48:44 +0530

    Understand export default & named ex[ort

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var a = 50;
-
+export var user = "Vikrant Singh"
+export var skills =['frontend', 'backend','UI/UX']
 
 export default a
--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-var h1 = document.createElement("h1")
+// var h1 = document.createElement("h1")
 
-h1.innerHTML = "Hello from JS!"
+// h1.innerHTML = "Hello from JS!"
 
 
-document.body.appendChild(h1)
+// document.body.appendChild(h1)
 
 
 // Library --> GSAPM, Lenis & REACTJS   ---> for particular feature
 // Framework ---> NEXTJS, Angular, Remix  --> 
 
 import a from './app.js'
+import {user, skills} from  "./app.js"
+
 console.log(a);
+console.log(user, skills);
+
