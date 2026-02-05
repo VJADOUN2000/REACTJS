479eb7dcdb74cba04dec3268fd96f47276cf88f7
vikrant singh <jadounvikrant@example.com>
2026-02-05 23:54:45 +0530

    Clear understanding of Export and import

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ export var user = "Vikrant Singh"
 export var skills =['frontend', 'backend','UI/UX']
 
 export default a
+
+// If we call or console any value by radom name the export default value will be called
+//but if we use {} then we have to use the exact name of the variable is called
