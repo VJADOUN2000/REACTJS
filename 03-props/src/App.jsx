a991bf23d8f408c57e252228ffec3d807aaeac2b
VJADOUN2000 <jadounvikrant@gmail.com>
2026-02-18 23:19:14 +0530

    mkae crd and dulpicte it and also make parent div

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -4,8 +4,21 @@ const App = () => {
   return (
     <div className='parent'>
       <div className="card">
+        <img src="https://images.unsplash.com/photo-1770064319432-9c5f134afca7?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         <h1>Vikrant Singh</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem harum accusamus ad?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>View Profile</button>
+      </div>
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1770064319432-9c5f134afca7?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Vikrant Singh</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>View Profile</button>
+      </div>
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1770064319432-9c5f134afca7?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Vikrant Singh</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
         <button>View Profile</button>
       </div>
     </div>
