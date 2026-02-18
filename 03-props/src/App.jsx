7bf3ab585b0f3697f4cad82fc4a236fc30a83466
VJADOUN2000 <jadounvikrant@gmail.com>
2026-02-18 23:29:01 +0530

    make a componts folder for card component

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import card from './components/card'
 
 const App = () => {
   return (
@@ -9,21 +10,9 @@ const App = () => {
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
         <button>View Profile</button>
       </div>
-      <div className="card">
-        <img src="https://images.unsplash.com/photo-1770064319432-9c5f134afca7?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <h1>Vikrant Singh</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <button>View Profile</button>
-      </div>
-      <div className="card">
-        <img src="https://images.unsplash.com/photo-1770064319432-9c5f134afca7?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <h1>Vikrant Singh</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <button>View Profile</button>
-      </div>
+      <card/>
     </div>
   )
 }
 
 export default App
-App
