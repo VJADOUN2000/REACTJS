ef64d3cf220ca08ddcc82e8bfe5366db71f87332
Vikrant Singh <jadounvikrant@gmail.com>
2026-04-03 23:28:16 +0530

    Work on props drilling

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -4,11 +4,11 @@ import Card from './components/card'
 const App = () => {
   return (
     <div className='parent'>
-      
-      <Card user='Ashish'/>
-      <Card/>
+      <div className="card">
+        <h1>Vikrant Singh</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
     </div>
-
   )
 }
 
