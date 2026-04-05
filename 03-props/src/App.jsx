37d118e24e94a8427087093dfdda37313d89f795
Vikrant Singh <jadounvikrant@gmail.com>
2026-04-05 21:39:30 +0530

    Maka mutiple new components using props and different data

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -4,30 +4,9 @@ import Card from './components/card'
 const App = () => {
   return (
     <div className='parent'>
-      <div className="card">
-        <img src="https://nanoreview.net/common/images/soc/qualcomm-mini.jpeg" alt="" />
-        <h1>Vikrant Singh</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        <button>View Proile</button>
-      </div>
-      <div className="card">
-        <img src="https://nanoreview.net/common/images/soc/qualcomm-mini.jpeg" alt="" />
-        <h1>Vikrant Singh</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        <button>View Proile</button>
-      </div>
-      <div className="card">
-        <img src="https://nanoreview.net/common/images/soc/qualcomm-mini.jpeg" alt="" />
-        <h1>Vikrant Singh</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        <button>View Proile</button>
-      </div>
-      <div className="card">
-        <img src="https://nanoreview.net/common/images/soc/qualcomm-mini.jpeg" alt="" />
-        <h1>Vikrant Singh</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        <button>View Proile</button>
-      </div>
+      <Card user='VJ' age={24} img= 'https://nanoreview.net/common/images/soc/qualcomm-mini.jpeg'/>
+      <Card user='Aman' age={26} img= 'https://images.unsplash.com/photo-1573496528681-9b0f4fb0c660?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8TWFjaGluZSUyMExlYXJuaW5nJTIwRW5naW5lZXJ8ZW58MHx8MHx8fDA%3D'/>
+      <Card user='Aditya' age={25} img='https://images.unsplash.com/photo-1581092163144-b7ae3c00adbc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fEVuZ2luZWVyfGVufDB8fDB8fHww'/>
     </div>
     
   )
--- 03-props/src/components/card.jsx
+++ 03-props/src/components/card.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function card(props){
 
-  console.log(props)
+  console.log(props.user, props.age)
   return (
-    <div className='card'>
-       <img src="https://images.unsplash.com/photo-1503376780353-7e6692767b70?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <h1>Vikrant Singh</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam.</p>
-        <button>View Profile</button>
-    </div>
+    <div className="card">
+        <img src={props.img} alt="" />
+        <h1>{props.user} {props.age}</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <button>View Proile</button>
+      </div>
   )
 }
 
