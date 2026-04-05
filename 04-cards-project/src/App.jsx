eab224bcb2e4ef1a6d99be5ff4aa5360c84546a6
Vikrant Singh <jadounvikrant@gmail.com>
2026-04-05 22:31:44 +0530

    Make a simple card job portal like sing reference from pinterest

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -11,11 +11,20 @@ const App = () => {
           <button>Save <Bookmark /></button>
         </div>
         <div className="center">
-
+            <h3>Amazon <span>5 Days ago</span></h3>
+            <h2>Senior Software Engineer</h2>
+            <div>
+              <h4>Part Time</h4>
+              <h4>Senior Level</h4>
+            </div>
         </div>
 
         <div className="bottom">
-
+            <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai, India</p>
+            </div>
+            <button>Apply Now</button>
         </div>
       </div>
 
