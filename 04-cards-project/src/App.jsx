b16dbd9adba58b9128547a9ec78e31514bbb4ab6
Vikrant Singh <jadounvikrant@gmail.com>
2026-04-06 15:40:54 +0530

    Complete card of amazon with different sections in app.jsx

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -6,17 +6,69 @@ const App = () => {
     <div className='parent'>
 
       <div className="card">
-        <div className="top">
+        <div>
+          <div className="top">
           <img src="https://www.freeiconspng.com/uploads/amazon-icon-3.png" alt="" />
           <button>Save <Bookmark size={10}/></button>
         </div>
         <div className="center">
             <h3>Amazon <span>5 Days ago</span></h3>
             <h2>Senior Software Engineer</h2>
-            <div>
+            <div className='tag'>
               <h4>Part Time</h4>
               <h4>Senior Level</h4>
             </div>
+        </div>
+        </div>
+
+        <div className="bottom">
+            <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai, India</p>
+            </div>
+            <button>Apply Now</button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div>
+          <div className="top">
+          <img src="https://www.freeiconspng.com/uploads/amazon-icon-3.png" alt="" />
+          <button>Save <Bookmark size={10}/></button>
+        </div>
+        <div className="center">
+            <h3>Amazon <span>5 Days ago</span></h3>
+            <h2>Senior Software Engineer</h2>
+            <div className='tag'>
+              <h4>Part Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+        </div>
+        </div>
+
+        <div className="bottom">
+            <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai, India</p>
+            </div>
+            <button>Apply Now</button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div>
+          <div className="top">
+          <img src="https://www.freeiconspng.com/uploads/amazon-icon-3.png" alt="" />
+          <button>Save <Bookmark size={10}/></button>
+        </div>
+        <div className="center">
+            <h3>Amazon <span>5 Days ago</span></h3>
+            <h2>Senior Software Engineer</h2>
+            <div className='tag'>
+              <h4>Part Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+        </div>
         </div>
 
         <div className="bottom">
