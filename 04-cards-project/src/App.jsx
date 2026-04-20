318d8ed0588ba07a9b73a4d69a2913d34b263d4c
Vikrant Singh <jadounvikrant@gmail.com>
2026-04-21 00:30:57 +0530

    Understanding React props and using elem in it

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -5,15 +5,65 @@ import User from './components/user'
 
 const App = () => {
 
-  const arr =[10,20,45]  
-
+  const jobOpenings = [
+  {
+    brandLogo: "https://logo.clearbit.com/meta.com",
+    companyName: "Meta",
+    datePosted: "5 days ago",
+    post: "Frontend Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$65/hour",
+    location: "Menlo Park, USA"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/amazon.com",
+    companyName: "Amazon",
+    datePosted: "2 days ago",
+    post: "Software Development Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$80/hour",
+    location: "Seattle, USA"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/apple.com",
+    companyName: "Apple",
+    datePosted: "1 week ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$95/hour",
+    location: "Cupertino, USA"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/netflix.com",
+    companyName: "Netflix",
+    datePosted: "3 days ago",
+    post: "Backend Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$110/hour",
+    location: "Los Gatos, USA"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/google.com",
+    companyName: "Google",
+    datePosted: "4 days ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$100/hour",
+    location: "Mountain View, USA"
+  }
+];
+  console.log(jobOpenings)
 
   return (
     <div className='parent'>
-       {arr.map(function(elem){
-          return <User name={elem}/>;
+       {jobOpenings.map(function(elem){
+          return <h1>{elem.location}</h1>
        })}
-       
     </div>
   )
 }
