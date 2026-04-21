f7d369d39745c30bd11c8493029f04819175c539
Vikrant Singh <jadounvikrant@gmail.com>
2026-04-21 19:07:52 +0530

    completed to chnage data of cards using props from array

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,60 +1,107 @@
-import React from 'react'
 import Card from './components/card'
-import User from './components/user'
-
 
 const App = () => {
 
   const jobOpenings = [
   {
-    brandLogo: "https://logo.clearbit.com/meta.com",
-    companyName: "Meta",
-    datePosted: "5 days ago",
-    post: "Frontend Engineer",
-    tag1: "Full Time",
-    tag2: "Junior Level",
-    pay: "$65/hour",
-    location: "Menlo Park, USA"
-  },
-  {
-    brandLogo: "https://logo.clearbit.com/amazon.com",
-    companyName: "Amazon",
-    datePosted: "2 days ago",
-    post: "Software Development Engineer",
-    tag1: "Full Time",
-    tag2: "Mid Level",
-    pay: "$80/hour",
-    location: "Seattle, USA"
-  },
-  {
-    brandLogo: "https://logo.clearbit.com/apple.com",
-    companyName: "Apple",
-    datePosted: "1 week ago",
-    post: "iOS Developer",
-    tag1: "Full Time",
-    tag2: "Senior Level",
-    pay: "$95/hour",
-    location: "Cupertino, USA"
-  },
-  {
-    brandLogo: "https://logo.clearbit.com/netflix.com",
-    companyName: "Netflix",
-    datePosted: "3 days ago",
-    post: "Backend Engineer",
-    tag1: "Full Time",
-    tag2: "Senior Level",
-    pay: "$110/hour",
-    location: "Los Gatos, USA"
-  },
-  {
-    brandLogo: "https://logo.clearbit.com/google.com",
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2sSeQqjaUTuZ3gRgkKjidpaipF_l6s72lBw&s",
     companyName: "Google",
-    datePosted: "4 days ago",
+    datePosted: "2 days ago",
     post: "Software Engineer",
     tag1: "Full Time",
     tag2: "Mid Level",
-    pay: "$100/hour",
-    location: "Mountain View, USA"
+    pay: "₹25-40 LPA",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png",
+    companyName: "Amazon",
+    datePosted: "1 day ago",
+    post: "SDE I",
+    tag1: "Full Time",
+    tag2: "Entry Level",
+    pay: "₹18-28 LPA",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://static.vecteezy.com/system/resources/thumbnails/027/127/473/small/microsoft-logo-microsoft-icon-transparent-free-png.png",
+    companyName: "Microsoft",
+    datePosted: "3 days ago",
+    post: "Software Engineer II",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "₹30-45 LPA",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+    companyName: "Apple",
+    datePosted: "5 days ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "₹40-60 LPA",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://w7.pngwing.com/pngs/153/31/png-transparent-netflix-macos-bigsur-icon-thumbnail.png",
+    companyName: "Netflix",
+    datePosted: "4 days ago",
+    post: "Backend Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "₹50-80 LPA",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://cdn.pixabay.com/photo/2021/12/14/22/29/meta-6871457_1280.png",
+    companyName: "Meta",
+    datePosted: "2 days ago",
+    post: "Frontend Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "₹35-55 LPA",
+    location: "Gurgaon, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2sSeQqjaUTuZ3gRgkKjidpaipF_l6s72lBw&s",
+    companyName: "Google",
+    datePosted: "6 days ago",
+    post: "Data Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "₹45-70 LPA",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png",
+    companyName: "Amazon",
+    datePosted: "3 days ago",
+    post: "SDE II",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "₹28-45 LPA",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://static.vecteezy.com/system/resources/thumbnails/027/127/473/small/microsoft-logo-microsoft-icon-transparent-free-png.png",
+    companyName: "Microsoft",
+    datePosted: "1 week ago",
+    post: "Cloud Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "₹30-50 LPA",
+    location: "Noida, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+    companyName: "Apple",
+    datePosted: "2 days ago",
+    post: "Machine Learning Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "₹50-75 LPA",
+    location: "Bangalore, India"
   }
 ];
   console.log(jobOpenings)
@@ -62,7 +109,7 @@ const App = () => {
   return (
     <div className='parent'>
        {jobOpenings.map(function(elem){
-          return <h1>{elem.location}</h1>
+          return <Card company={elem.companyName} post ={elem.post} tag1 ={elem.tag1} tag2={elem.tag2} pay={elem.pay} location={elem.location} brandLogo={elem.brandLogo} datePosted={elem.datePosted}/>
        })}
     </div>
   )
--- 04-cards-project/src/components/card.jsx
+++ 04-cards-project/src/components/card.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import {Bookmark} from 'lucide-react'
 
-const card = () => {
+const card = (props) => {
+
+  console.log(props.company)
   return (
     <div className="card">
         <div>
           <div className="top">
-          <img src="https://www.freeiconspng.com/uploads/amazon-icon-3.png" alt="" />
+          <img src={props.brandLogo} alt="" />
           <button>Save <Bookmark size={10}/></button>
         </div>
         <div className="center">
-            <h3>Amazon <span>5 Days ago</span></h3>
-            <h2>Senior Software Engineer</h2>
+            <h3>{props.company} <span>{props.datePosted}</span></h3>
+            <h2>{props.post}</h2>
             <div className='tag'>
-              <h4>Part Time</h4>
-              <h4>Senior Level</h4>
+              <h4>{props.tag1}</h4>
+              <h4>{props.tag2}</h4>
             </div>
         </div>
         </div>
 
         <div className="bottom">
             <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai, India</p>
+                <h3>{props.pay}</h3>
+                <p>{props.location}</p>
             </div>
             <button>Apply Now</button>
         </div>
