b616ba90a18270e0eded9fe4e86a0bd706085c50
Vikrant Singh <jadounvikrant@gmail.com>
2026-04-29 17:08:35 +0530

    Done with Navbar components

--- 07-ui-project/src/components/Section1/Navbar.jsx
+++ 07-ui-project/src/components/Section1/Navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between px-12 py-4'>
-     <h4 className='bg-black text-white px-3 py-3 rounded-full'>Target Audience</h4>
-     <button>Digital Ocean Platform</button>
+     <h4 className='bg-black text-white px-6 py-3 rounded-full'>Target Audience</h4>
+     <button className='bg-gray-300 px-3 py-2 rounded-full uppercase font-medium tracking-widest text-sm'>Digital Ocean Platform</button>
     </div>
   )
 }
