fc065d0b4858c2be6498ec08c9d42c7cb957e8b1
Vikrant Singh <jadounvikrant@gmail.com>
2026-04-29 16:47:15 +0530

    Working on UI Projects and made two sections and a navbar

---
+++ 07-ui-project/src/components/Section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-12 py-4'>
+     <h4 className='bg-black text-white px-3 py-3 rounded-full'>Target Audience</h4>
+     <button>Digital Ocean Platform</button>
+    </div>
+  )
+}
+
+export default Navbar
