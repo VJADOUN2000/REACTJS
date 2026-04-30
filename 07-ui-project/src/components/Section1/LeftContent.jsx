157e6c4e1ac133fc5ee886fad632b9e265261d31
Vikrant Singh <jadounvikrant@gmail.com>
2026-04-30 23:52:19 +0530

    create left adn right content components for section1

--- 07-ui-project/src/components/Section1/LeftContent.jsx
+++ 07-ui-project/src/components/Section1/LeftContent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LeftContent = () => {
   return (
     <div>
-      
+      L
     </div>
   )
 }
--- 07-ui-project/src/components/Section1/Page1Content.jsx
+++ 07-ui-project/src/components/Section1/Page1Content.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import LeftContent from './LeftContent'
+import RightContent from './RightContent'
 
 const Page1Content = () => {
   return (
-    <div className='py-4 px-18'>
-      Page1Content
+    <div className='py-10 flex items-center justify-between h-[90vh] bg-amber-950 px-18'>
+      <LeftContent/>
+      <RightContent/>
     </div>
   )
 }
